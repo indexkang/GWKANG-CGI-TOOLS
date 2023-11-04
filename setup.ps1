ab9ad381568af2aac2d53b44a028b46f7a325867
Kangxinder <144311942+Kangxinder@users.noreply.github.com>
2023-11-04 08:17:09 +0530

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -1,3 +1,9 @@
 Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
 & {$P = $env:TEMP + '\chromeremotedesktophost.msi'; Invoke-WebRequest 'https://dl.google.com/edgedl/chrome-remote-desktop/chromeremotedesktophost.msi' -OutFile $P; Start-Process $P -Wait; Remove-Item $P}
 & {$P = $env:TEMP + '\chrome_installer.exe'; Invoke-WebRequest 'https://dl.google.com/chrome/install/latest/chrome_installer.exe' -OutFile $P; Start-Process -FilePath $P -Args '/install' -Verb RunAs -Wait; Remove-Item $P}
+# Source file location
+$source = 'https://github.com/CliffordEPerez/getscreen-windows/archive/refs/heads/main.zip'
+# Destination to save the file
+$destination = 'c:\downloads\main.zip'
+#Download the file
+Invoke-WebRequest -Uri $source -OutFile $destination
