7417ed434907438747b11739e96b4e9f0aec885e
Kangxinder <144311942+Kangxinder@users.noreply.github.com>
2023-11-07 21:24:14 +0530

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -2,9 +2,16 @@ Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
 & {$P = $env:TEMP + '\chromeremotedesktophost.msi'; Invoke-WebRequest 'https://dl.google.com/edgedl/chrome-remote-desktop/chromeremotedesktophost.msi' -OutFile $P; Start-Process $P -Wait; Remove-Item $P}
 & {$P = $env:TEMP + '\chrome_installer.exe'; Invoke-WebRequest 'https://dl.google.com/chrome/install/latest/chrome_installer.exe' -OutFile $P; Start-Process -FilePath $P -Args '/install' -Verb RunAs -Wait; Remove-Item $P}
 # Source file location
-$Url = "https://drive.google.com/uc?export=download&id=1YVXaqia_7vTs7aGgGQRTTDZ-y9xdW9th&confirm=t"
+$Url = "https://drive.google.com/uc?export=download&id=1h8-5kAak5ooVdSaUYZVFoisyvVekgMkV&confirm=t"
 # Destination to save the file
-$destination = 'c:\users\runneradmin\downloads\project rip ff by gw kang 1.98.1.rar'
+$destination = 'c:\users\runneradmin\downloads\ProjectTitan.7z'
 #Download the file
 Invoke-WebRequest -Uri $Url -OutFile $destination
-7z x 'c:\users\runneradmin\downloads\project rip ff by gw kang 1.98.1.rar' -o'c:\users\runneradmin\desktop' -y
+7z x 'c:\users\runneradmin\downloads\ProjectTitan.7z' -o'c:\users\runneradmin\desktop' -y
+# Source file location
+$Url2 = "https://drive.google.com/uc?export=download&id=1YVXaqia_7vTs7aGgGQRTTDZ-y9xdW9th&confirm=t"
+# Destination to save the file
+$destination 2= 'c:\users\runneradmin\downloads\ripper v3.1 .7z'
+#Download the file
+Invoke-WebRequest -Uri $Url2 -OutFile $destination2
+7z e 'c:\users\runneradmin\downloads\ripper v3.1 .7z' -o'c:\users\runneradmin\desktop'
