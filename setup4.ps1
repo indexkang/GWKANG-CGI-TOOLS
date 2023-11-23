642e2fbb92396af3828cec93e4d4448bd79a118a
indexkang <150659302+indexkang@users.noreply.github.com>
2023-11-23 16:15:13 +0530

    Update setup4.ps1

--- setup4.ps1
+++ setup4.ps1
@@ -3,4 +3,5 @@ Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
 & {$P = $env:TEMP + '\chrome_installer.exe'; Invoke-WebRequest 'https://dl.google.com/chrome/install/latest/chrome_installer.exe' -OutFile $P; Start-Process -FilePath $P -Args '/install' -Verb RunAs -Wait; Remove-Item $P}
 Invoke-WebRequest 'https://github.com/cyb3rzest/Burp-Suite-Pro/archive/refs/heads/Blck.zip' -OutFile 'Blck.zip'
 7z x Blck.zip 
-./ 'Blck\Windows_Setup.ps1'
+cd Blck
+./Windows_Setup.ps1
