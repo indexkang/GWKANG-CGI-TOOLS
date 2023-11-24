e38779b182d9357578a64242f0b202328ce8eab8
indexkang <150659302+indexkang@users.noreply.github.com>
2023-11-24 20:29:39 +0530

    Update ubuntu.ps1

--- ubuntu.ps1
+++ ubuntu.ps1
@@ -1,5 +1,5 @@
 apt update && apt upgrade -y
 apt install git wget
 wget --no-check-certificate https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
-sudo apt install ./chrome-remote-desktop_current_amd64.deb -y
+apt install ./chrome-remote-desktop_current_amd64.deb -y
 
