1fdb75fc44770e440b81781889c74d5e32cd3c96
indexkang <150659302+indexkang@users.noreply.github.com>
2023-12-06 19:51:19 +0530

    Create devx.ps1

---
+++ devx.ps1
@@ -0,0 +1,2 @@
+Invoke-WebRequest 'https://github.com/Polarmods/DevX-Cracked/releases/download/1.0.9/DevXCrack_1.0.9.7z' -OutFile 'C:\users\runneradmin\desktop\devx'
+7z x -o"C:\users\runneradmin\desktop\"
