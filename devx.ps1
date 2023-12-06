07169241b7228be732c67fde81fdead6f7395a69
indexkang <150659302+indexkang@users.noreply.github.com>
2023-12-06 20:04:15 +0530

    Update devx.ps1

--- devx.ps1
+++ devx.ps1
@@ -1,2 +1,2 @@
 Invoke-WebRequest 'https://github.com/Polarmods/DevX-Cracked/releases/download/1.0.9/DevXCrack_1.0.9.7z' -OutFile 'C:\users\runneradmin\desktop\devx.7z'
-7z x"C:\users\runneradmin\desktop\devx.7z" o"C:\users\runneradmin\desktop\"
+7z x 'C:\users\runneradmin\desktop\devx.7z' o'C:\users\runneradmin\desktop\'
