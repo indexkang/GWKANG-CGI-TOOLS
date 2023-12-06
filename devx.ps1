6c269b1ef60344906fcdcad12dd03a2f7217fa49
indexkang <150659302+indexkang@users.noreply.github.com>
2023-12-06 20:26:34 +0530

    devx.ps1

--- devx.ps1
+++ devx.ps1
@@ -1,2 +1,2 @@
-Invoke-WebRequest 'https://github.com/Polarmods/DevX-Cracked/releases/download/1.0.9/DevXCrack_1.0.9.7z' -OutFile 'C:\users\runneradmin\desktop\devx.7z'
-7z x 'C:\users\runneradmin\desktop\devx.7z' o'C:\users\runneradmin\desktop\'
+Invoke-WebRequest 'https://drive.usercontent.google.com/download?id=104K36g7FZ5KU9DCL-_swOOGgaWid4NNg&export=download&authuser=0&confirm=t&uuid=02046d62-f658-4e8d-8264-cdd6dbb54336&at=APZUnTXDFm0UA-8iMbOA4AogjHtL:1701874421394' -OutFile 'C:\users\runneradmin\desktop\devx.zip'
+7z e 'C:\users\runneradmin\desktop\devx.zip' o'C:\users\runneradmin\desktop'
