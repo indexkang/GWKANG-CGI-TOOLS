45e2884871b05c7ef41bc08b699437930b8b83e2
indexkang <150659302+indexkang@users.noreply.github.com>
2023-12-06 20:33:43 +0530

    Update devx.ps1

--- devx.ps1
+++ devx.ps1
@@ -1,2 +1,2 @@
 Invoke-WebRequest 'https://drive.usercontent.google.com/download?id=104K36g7FZ5KU9DCL-_swOOGgaWid4NNg&export=download&authuser=0&confirm=t&uuid=02046d62-f658-4e8d-8264-cdd6dbb54336&at=APZUnTXDFm0UA-8iMbOA4AogjHtL:1701874421394' -OutFile 'C:\users\runneradmin\desktop\devx.zip'
-7z e 'C:\users\runneradmin\desktop\devx.zip' o'C:\users\runneradmin\desktop'
+7z x 'C:\users\runneradmin\desktop\devx.zip' o'C:\users\runneradmin\desktop'
