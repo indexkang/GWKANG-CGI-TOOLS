9cc918e2cdc5d9c003c1c4b05eb278ddd8693961
indexkang <150659302+indexkang@users.noreply.github.com>
2023-12-06 20:05:49 +0530

    Delete ubuntu.ps1

--- ubuntu.ps1
+++
@@ -1,5 +0,0 @@
-apt update && apt upgrade -y
-apt install git wget
-wget --no-check-certificate https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
-apt install ./chrome-remote-desktop_current_amd64.deb -y
-
