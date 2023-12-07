93e12d8869dd3c12f92a290be51699235bf92883
indexkang <150659302+indexkang@users.noreply.github.com>
2023-12-07 18:06:48 +0530

    Update devx.ps1

--- devx.ps1
+++ devx.ps1
@@ -1,3 +1,3 @@
-cd c:\user\runneradmin\desktop
+cd c:\users\runneradmin\desktop
 Invoke-WebRequest 'https://drive.usercontent.google.com/download?id=11PcqD2gpEVqntPCQrYoUS6xtg3oD4HdU&export=download&authuser=0&confirm=t&uuid=4f63c0cc-636a-46fd-a02c-1ea2a846fd39&at=APZUnTVBhHovVh_1xMEx_HhAVx5Z:1701951830717'
 7z x DevXCrack_1.0.9.7z
