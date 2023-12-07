60447f0d03ddb97616f11eb1ca29193564d409b5
indexkang <150659302+indexkang@users.noreply.github.com>
2023-12-07 18:42:37 +0530

    Update devx.ps1

--- devx.ps1
+++ devx.ps1
@@ -1,3 +1,3 @@
 cd c:\users\runneradmin\desktop
-Invoke-WebRequest 'https://github.com/indexkang/GWKANG-CGI-TOOLS/releases/download/Devx/devx.7z'
-7z x DevXCrack_1.0.9.7z
+Invoke-WebRequest 'https://github.com/indexkang/GWKANG-CGI-TOOLS/releases/download/Devx/devx.7z' -OutFile 'devx.7z'
+7z x devx.7z
