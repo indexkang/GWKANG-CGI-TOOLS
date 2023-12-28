08c6c3ff1edb06d7a66336bc24e1e250fe55dc6a
indexkang <150659302+indexkang@users.noreply.github.com>
2023-12-28 12:34:08 +0530

    Update blender.ps1

--- blender.ps1
+++ blender.ps1
@@ -1,1 +1,2 @@
-Invoke-WebRequest 'https://download.blender.org/release/Blender2.93/blender-2.93.9-windows-x64.msi' 
+Invoke-WebRequest 'https://download.blender.org/release/Blender2.93/blender-2.93.9-windows-x64.msi' -OutFile 'blender.msi'
+start-process 'blender.msi'
