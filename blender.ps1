9f170b81f9bda606ebb9888d8156f2ebec19516c
indexkang <150659302+indexkang@users.noreply.github.com>
2023-12-29 15:49:44 +0530

    Update blender.ps1

--- blender.ps1
+++ blender.ps1
@@ -1,2 +1,2 @@
-$Url = "https://drive.google.com/uc?export=download&id=11xjuX4Pd0oQNBscM86DmKPHu_urCEciZ&confirm=t"
+$Url = "https://drive.google.com/uc?export=download&id=1ZP0-bhA4eF7c7Xx9Leg1BWwI2KUCBrsn&confirm=t"
 $Destination = 'C:\users\runneradmin\desktop\blender.zip'
