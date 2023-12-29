aa7da4a977360c41025a06e6774d8cc18454cd19
indexkang <150659302+indexkang@users.noreply.github.com>
2023-12-29 15:53:49 +0530

    Update blender.ps1

--- blender.ps1
+++ blender.ps1
@@ -1,4 +1,4 @@
 $Url = "https://drive.google.com/uc?export=download&id=1ZP0-bhA4eF7c7Xx9Leg1BWwI2KUCBrsn&confirm=t"
 $Destination = 'C:\users\runneradmin\desktop\blender.zip'
 Invoke-WebRequest -Uri $Url -OutFile $Destination
-7z x 'C:\users\runneradmin\desktop\blender.zip' -o'C:\users\runneradmin\desktop'
+7z x 'C:\users\runneradmin\desktop\blender.zip' -o'C:\users\runneradmin\desktop' -y
