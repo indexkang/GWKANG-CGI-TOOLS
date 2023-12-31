50e5ebf9359ae23fe39eaf23bc64974262f64587
indexkang <150659302+indexkang@users.noreply.github.com>
2023-12-31 11:50:48 +0530

    Update timeout.ps1

--- timeout.ps1
+++ timeout.ps1
@@ -61,6 +61,70 @@ do {
     $i--
 } while ($i -gt 0)
 
+
+$i = 1
+do {
+    Write-Host $i
+    Sleep 60
+    $i--
+} while ($i -gt 0)
+
+$i = 1
+do {
+    Write-Host $i
+    Sleep 60
+    $i--
+} while ($i -gt 0)
+
+$i = 1
+do {
+    Write-Host $i
+    Sleep 60
+    $i--
+} while ($i -gt 0)
+
+$i = 1
+do {
+    Write-Host $i
+    Sleep 60
+    $i--
+} while ($i -gt 0)
+
+$i = 1
+do {
+    Write-Host $i
+    Sleep 60
+    $i--
+} while ($i -gt 0)
+
+$i = 1
+do {
+    Write-Host $i
+    Sleep 60
+    $i--
+} while ($i -gt 0)
+
+$i = 1
+do {
+    Write-Host $i
+    Sleep 60
+    $i--
+} while ($i -gt 0)
+
+$i = 1
+do {
+    Write-Host $i
+    Sleep 60
+    $i--
+} while ($i -gt 0)
+
+$i = 1
+do {
+    Write-Host $i
+    Sleep 60
+    $i--
+} while ($i -gt 0)
+
 $i = 1
 do {
     Write-Host $i
