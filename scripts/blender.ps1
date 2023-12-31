8e410a68292b2e56f7bacc3b54f7ef6f3980cc54
indexkang <150659302+indexkang@users.noreply.github.com>
2023-12-31 11:44:16 +0530

    Update blender.ps1

--- scripts/blender.ps1
+++ scripts/blender.ps1
@@ -1,4 +1,3 @@
-$Url = "https://drive.google.com/uc?export=download&id=1ZP0-bhA4eF7c7Xx9Leg1BWwI2KUCBrsn&confirm=t"
-$Destination = 'C:\users\runneradmin\desktop\blender.zip'
-Invoke-WebRequest -Uri $Url -OutFile $Destination
-7z x 'C:\users\runneradmin\desktop\blender.zip' -o'C:\users\runneradmin\desktop' -y
+cd C:\users\runneradmin\desktop
+Invoke-Webrequest "https://drive.google.com/uc?export=download&id=1ZP0-bhA4eF7c7Xx9Leg1BWwI2KUCBrsn&confirm=t" -OutFile "blender.zip"
+7z x blender.zip
