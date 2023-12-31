6b081ca3accdfd46657f0b2688d59bf9aa28094c
indexkang <150659302+indexkang@users.noreply.github.com>
2023-12-31 11:47:52 +0530

    Update ff.ps1

--- scripts/ff.ps1
+++ scripts/ff.ps1
@@ -1,7 +1,2 @@
-# Source file location
-$Url = "https://drive.google.com/uc?export=download&id=11xjuX4Pd0oQNBscM86DmKPHu_urCEciZ&confirm=t"
-# Destination to save the file
-$destination = 'c:\users\runneradmin\downloads\free fire ripper by Gw KANG.7z'
-#Download the file
-Invoke-WebRequest -Uri $Url -OutFile $destination
-7z x 'c:\users\runneradmin\downloads\free fire ripper by Gw KANG.7z' -o'c:\users\runneradmin\desktop' -y
+cd C:\users\runneradmin\desktop Invoke-WebRequest "https://drive.google.com/uc?export=download&id=11xjuX4Pd0oQNBscM86DmKPHu_urCEciZ&confirm=t" -OutFile 'free fire ripper by Gw KANG.7z'
+7z x free fire ripper by Gw KANG.7z 
