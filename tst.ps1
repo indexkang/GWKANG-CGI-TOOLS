1098a8d953de4ebb342348429194f31f9ba1d455
indexkang <150659302+indexkang@users.noreply.github.com>
2024-01-09 17:39:59 +0530

    Update tst.ps1

--- tst.ps1
+++ tst.ps1
@@ -1,6 +1,6 @@
 $Url = "https://accounts.google.com/v3/signin/identifier?authuser=0&continue=https%3A%2F%2Fwww.google.com%2Fsearch%3Fq%3Dgoogle%26oq%3Dgoogle%26gs_lcrp%3DEgZjaHJvbWUyBggAEEUYOTITCAEQLhiDARjHARixAxjRAxiABDIGCAIQRRg8MgYIAxBFGDwyBggEEEUYPDIGCAUQRRg7MgYIBhBFGDsyBggHEEUYOzIECAgQBTIGCAkQBRgs0gEIMzI0OGowajSoAgCwAgA%26sourceid%3Dchrome-mobile%26ie%3DUTF-8%26hl%3Den&ec=GAlAAQ&hl=en&flowName=GlifWebSignIn&flowEntry=AddSession&dsh=S-1962165682%3A1704801783179634&theme=glif"; 
-$Username="id5inder5@gmail.com";
-$Password="55555inder55555";
+$Username="inderpreet0855@gmail.com";
+$Password="opkang123";
 
 $Credential = New-Object System.Management.Automation.PSCredential -ArgumentList @($Username,(ConvertTo-SecureString -String $Password -AsPlainText -Force))
 
