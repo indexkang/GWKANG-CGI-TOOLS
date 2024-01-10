3a33d5d5b577e3e9c699a82781cac9bc06b07646
indexkang <150659302+indexkang@users.noreply.github.com>
2024-01-10 17:24:12 +0530

    Update unity.ps1

--- scripts/unity.ps1
+++ scripts/unity.ps1
@@ -1,9 +1,9 @@
 cd 'C:\Program Files'
-Invoke-WebRequest -Uri https://drive.google.com/uc?export=download&id=1skKg1HFVimsAHQj0nm8AC5WkRtL0KsG8&confirm=t -OutFile 'Unity Hub.zip'
+Invoke-WebRequest -Uri 'https://drive.google.com/uc?export=download&id=1skKg1HFVimsAHQj0nm8AC5WkRtL0KsG8&confirm=t' -OutFile 'Unity Hub.zip'
 7z x Unity Hub.zip -y
 
 cd 'C:\Program Data'
-Invoke-WebRequest -Uri https://drive.google.com/uc?export=download&id=13DSiqvkEL257MwZYwnpwtnRve47VnCua&confirm=t -OutFile 'Unity.7z'
+Invoke-WebRequest -Uri 'https://drive.google.com/uc?export=download&id=13DSiqvkEL257MwZYwnpwtnRve47VnCua&confirm=t' -OutFile 'Unity.7z'
 7z x Unity.7z -y
 
 cd 'C:\users\runneradmin\appdata\local'
