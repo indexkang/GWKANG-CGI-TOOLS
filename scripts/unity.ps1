01fabe19233b973c35b29959bdce8035bc82a975
indexkang <150659302+indexkang@users.noreply.github.com>
2024-01-10 13:27:58 +0530

    Update unity.ps1

--- scripts/unity.ps1
+++ scripts/unity.ps1
@@ -1,4 +1,4 @@
-cd 'C:\users\runneradmin\Dektop'
+cd 'C:\users\runneradmin\desktop'
 $url= 'https://drive.google.com/uc?export=download&id=1mq2WTp77RDv7lL2gW6CHSuZhK0yzwVpU&confirm=t'
 $p='unity.zip'
 
