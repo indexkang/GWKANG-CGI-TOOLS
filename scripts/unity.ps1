dc93bce4391d3f9768786f5f33ff9dc598458be4
indexkang <150659302+indexkang@users.noreply.github.com>
2024-01-10 17:33:34 +0530

    Update unity.ps1

--- scripts/unity.ps1
+++ scripts/unity.ps1
@@ -2,10 +2,10 @@ cd 'C:\Program Files'
 Invoke-WebRequest -Uri 'https://drive.google.com/uc?export=download&id=1skKg1HFVimsAHQj0nm8AC5WkRtL0KsG8&confirm=t' -OutFile 'Unity Hub.zip'
 7z x 'Unity Hub.zip' -y
 
-cd 'C:\Program Data'
+cd 'C:\ProgramData'
 Invoke-WebRequest -Uri 'https://drive.google.com/uc?export=download&id=13DSiqvkEL257MwZYwnpwtnRve47VnCua&confirm=t' -OutFile 'Unity.7z'
 7z x 'Unity.7z' -y
 
-cd 'C:\users\runneradmin\appdata\local'
+cd 'C:\users\runneradmin\AppData\local'
 Invoke-Webrequest -Uri 'https://drive.google.com/uc?export=download&id=1wSvdU0FzPa4rFEo56HyV4gMGvgIaxLC2&confirm=t' -OutFile 'unity.zip'
 7z x 'Unity.zip' -y
