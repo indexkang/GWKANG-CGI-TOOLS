845f9dd61b3d3af37c2d8b724e024f779a71dc59
indexkang <150659302+indexkang@users.noreply.github.com>
2024-01-10 13:24:26 +0530

    Update unity.ps1

--- scripts/unity.ps1
+++ scripts/unity.ps1
@@ -1,6 +1,6 @@
 cd 'C:\users\runneradmin\Dektop'
-$url= 
-'https://drive.google.com/uc?export=download&id=1mq2WTp77RDv7lL2gW6CHSuZhK0yzwVpU&confirm=t'
-$p= 'unity'
+$url= 'https://drive.google.com/uc?export=download&id=1mq2WTp77RDv7lL2gW6CHSuZhK0yzwVpU&confirm=t'
+$p='unity.zip'
 
 Invoke-WebRequest -Uri $url -OutFile $p
+7z x $p
