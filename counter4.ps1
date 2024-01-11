c387fb659f504113e11b371ee994701aa5a958d6
indexkang <150659302+indexkang@users.noreply.github.com>
2024-01-11 17:44:40 +0530

    Update

---
+++ counter4.ps1
@@ -0,0 +1,84 @@
+$i = 1
+do {
+    Write-Host $i
+    Sleep 300
+    $i--
+} while ($i -gt 0)
+
+
+$i = 1
+do {
+    Write-Host $i
+    Sleep 300
+    $i--
+} while ($i -gt 0)
+
+$i = 1
+do {
+    Write-Host $i
+    Sleep 300
+    $i--
+} while ($i -gt 0)
+
+$i = 1
+do {
+    Write-Host $i
+    Sleep 300
+    $i--
+} while ($i -gt 0)
+
+$i = 1
+do {
+    Write-Host $i
+    Sleep 300
+    $i--
+} while ($i -gt 0)
+
+$i = 1
+do {
+    Write-Host $i
+    Sleep 300
+    $i--
+} while ($i -gt 0)
+
+$i = 1
+do {
+    Write-Host $i
+    Sleep 300
+    $i--
+} while ($i -gt 0)
+
+$i = 1
+do {
+    Write-Host $i
+    Sleep 300
+    $i--
+} while ($i -gt 0)
+
+$i = 1
+do {
+    Write-Host $i
+    Sleep 300
+    $i--
+} while ($i -gt 0)
+
+$i = 1
+do {
+    Write-Host $i
+    Sleep 300
+    $i--
+} while ($i -gt 0)
+
+$i = 1
+do {
+    Write-Host $i
+    Sleep 300
+    $i--
+} while ($i -gt 0)
+
+$i = 1
+do {
+    Write-Host $i
+    Sleep 300
+    $i--
+} while ($i -gt 0)
