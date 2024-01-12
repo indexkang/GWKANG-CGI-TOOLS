40e4131598e2ee95dc5bb41422da6aa0e978e9c5
indexkang <150659302+indexkang@users.noreply.github.com>
2024-01-12 18:07:32 +0530

    Update Winfile.ps1

--- scripts/Winfile.ps1
+++ scripts/Winfile.ps1
@@ -1,2 +1,2 @@
 $blender = '${{ github.event.inputs.name }}'
-Invoke-WebRequest -Uri "https://download.blender.org/release/Blender$blender/blender-$blender-windows32.zip" -OutFile '$blender'
+Invoke-WebRequest "https://download.blender.org/release/Blender $blender /blender- $blender -windows32.zip" -OutFile '$blender'
