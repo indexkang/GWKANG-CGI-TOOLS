3495500c674aaa9a84c5b2d891d0a9ac11d571d4
indexkang <150659302+indexkang@users.noreply.github.com>
2024-01-12 09:17:26 +0530

    Update Winfile.ps1

--- scripts/Winfile.ps1
+++ scripts/Winfile.ps1
@@ -1,10 +1,9 @@
 $s= '${{ github.event.inputs.name }}'
 echo 'hiiiii'
 if ( $s -eq 'windows') {
-    "$p= hii"
-}
-if ( $s -eq 'win') {
-    "$p= wii"
+     "$p= hii"
+} if ( $s -eq 'win') {
+     "$p= wii"
 } else {
      "$p= bye"
 }
