7a4d59ffea2b1bd154a11ce220dc57dbeecca817
indexkang <150659302+indexkang@users.noreply.github.com>
2024-01-12 17:46:28 +0530

    Update blender.ps1

--- scripts/blender.ps1
+++ scripts/blender.ps1
@@ -1,3 +1,5 @@
+$blender= '${{ github.event.inputs.version }}
+Invoke-WebRequest -Uri 'https://download.blender.org/release/Blender$blender/blender-$blender-windows32.zip'
 cd C:\users\runneradmin\desktop
 Invoke-Webrequest "https://drive.google.com/uc?export=download&id=1ZP0-bhA4eF7c7Xx9Leg1BWwI2KUCBrsn&confirm=t" -OutFile "blender.zip"
 7z x blender.zip
