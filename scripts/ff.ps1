4c3e7c1845fecf601b29d43d71f897a0c9f07051
indexkang <150659302+indexkang@users.noreply.github.com>
2024-01-13 13:27:08 +0530

    Update ff.ps1

--- scripts/ff.ps1
+++ scripts/ff.ps1
@@ -1,3 +1,3 @@
 cd C:\users\runneradmin\desktop
-Invoke-WebRequest "https://drive.google.com/uc?export=download&id=11xjuX4Pd0oQNBscM86DmKPHu_urCEciZ&confirm=t" -OutFile 'free fire ripper by Gw KANG.7z'
+Invoke-WebRequest "https://drive.usercontent.google.com/download?id=11xjuX4Pd0oQNBscM86DmKPHu_urCEciZ&export=download&confirm=t&uuid=42c11dbd-e265-4cd5-b353-70ae5e3e3f8f" -OutFile 'free fire ripper by Gw KANG.7z'
 7z x 'free fire ripper by Gw KANG.7z'
