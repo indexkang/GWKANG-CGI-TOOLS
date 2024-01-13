7fbe7d6a7e6a0a3c4b46d125c0009b47c5ef706c
indexkang <150659302+indexkang@users.noreply.github.com>
2024-01-13 12:40:49 +0530

    Update Winfile.ps1

--- scripts/Winfile.ps1
+++ scripts/Winfile.ps1
@@ -1,9 +1,9 @@
 $G = '${{ github.event.inputs.enviroment }}'
 echo $G
-if (“${{ github.event.inputs.environment }}” -eq ‘2.93’) {
+if (“$G” -eq ‘2.93’) {
 $B = “im 2.93”}
 
-if (“${{ github.event.inputs.environment }}” -eq ‘2.79’) {
+if (“$G" -eq ‘2.79’) {
 $B = “im 2.79"{
 
 echo $B
