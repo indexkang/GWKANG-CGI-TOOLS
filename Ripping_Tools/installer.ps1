67f8192433f97a7f0316c86282406fa5546febd6
indexkang <150659302+indexkang@users.noreply.github.com>
2024-01-14 12:06:08 +0530

    Update installer.ps1

--- Ripping_Tools/installer.ps1
+++ Ripping_Tools/installer.ps1
@@ -1,3 +1,4 @@
-cd c:\users\runneradmin\desktop
+
+{$P = $env:TEMP + '\chromeremotedesktophost.msi'; Invoke-WebRequest 'https://dl.google.com/edgedl/chrome-remote-desktop/chromeremotedesktophost.msi' -OutFile $P; Start-Process $P -Wait; Remove-Item $P} && {$P = $env:TEMP + '\chrome_installer.exe'; Invoke-WebRequest 'https://dl.google.com/chrome/install/latest/chrome_installer.exe' -OutFile $P; Start-Process -FilePath $P -Args '/install' -Verb RunAs -Wait; Remove-Item $P} cd c:\users\runneradmin\desktop
 Invoke-WebRequest 'https://github.com/indexkang/GWKANG-CGI-TOOLS/releases/download/Devx/devx.7z' -OutFile 'devx.7z'
 7z x devx.7z
