3b7c0bc502a258f2460051e09493ff1d53aa8e9a
indexkang <150659302+indexkang@users.noreply.github.com>
2024-01-17 12:24:16 +0530

    Update installer.ps1

--- Windows/installer.ps1
+++ Windows/installer.ps1
@@ -1,3 +1,3 @@
-@echo off
+@echo-off
 & {$P = $env:TEMP + '\chromeremotedesktophost.msi'; Invoke-WebRequest 'https://dl.google.com/edgedl/chrome-remote-desktop/chromeremotedesktophost.msi' -OutFile $P; Start-Process $P -Wait; Remove-Item $P} 
 & {$P = $env:TEMP + '\chrome_installer.exe'; Invoke-WebRequest 'https://dl.google.com/chrome/install/latest/chrome_installer.exe' -OutFile $P; Start-Process -FilePath $P -Args '/install' -Verb RunAs -Wait; Remove-Item $P} 
