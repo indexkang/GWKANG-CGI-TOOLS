e1610ebe0ad3ac17f25b81bed026c3329bac0900
indexkang <150659302+indexkang@users.noreply.github.com>
2024-01-22 15:13:39 +0530

    Update counter2.ps1

--- counter2.ps1
+++ counter2.ps1
@@ -1,7 +1,14 @@
 $i = 1
 do {
     Write-Host $i
-    Sleep 300
+    Sleep 60
+    $i--
+} while ($i -gt 0)
+
+$i = 1
+do {
+    Write-Host $i
+    Sleep 120
     $i--
 } while ($i -gt 0)
 
@@ -9,76 +16,206 @@ do {
 $i = 1
 do {
     Write-Host $i
-    Sleep 300
+    Sleep 180
+    $i--
+} while ($i -gt 0)
+
+
+$i = 1
+do {
+    Write-Host $i
+    Sleep 60
     $i--
 } while ($i -gt 0)
 
 $i = 1
 do {
     Write-Host $i
-    Sleep 300
+    Sleep 120
+    $i--
+} while ($i -gt 0)
+
+
+$i = 1
+do {
+    Write-Host $i
+    Sleep 180
     $i--
 } while ($i -gt 0)
 
 $i = 1
 do {
     Write-Host $i
-    Sleep 300
+    Sleep 60
     $i--
 } while ($i -gt 0)
 
 $i = 1
 do {
     Write-Host $i
-    Sleep 300
+    Sleep 120
+    $i--
+} while ($i -gt 0)
+
+
+$i = 1
+do {
+    Write-Host $i
+    Sleep 180
     $i--
 } while ($i -gt 0)
 
 $i = 1
 do {
     Write-Host $i
-    Sleep 300
+    Sleep 60
     $i--
 } while ($i -gt 0)
 
 $i = 1
 do {
     Write-Host $i
-    Sleep 300
+    Sleep 120
+    $i--
+} while ($i -gt 0)
+
+
+$i = 1
+do {
+    Write-Host $i
+    Sleep 180
     $i--
 } while ($i -gt 0)
 
 $i = 1
 do {
     Write-Host $i
-    Sleep 300
+    Sleep 60
     $i--
 } while ($i -gt 0)
 
 $i = 1
 do {
     Write-Host $i
-    Sleep 300
+    Sleep 120
+    $i--
+} while ($i -gt 0)
+
+
+$i = 1
+do {
+    Write-Host $i
+    Sleep 180
     $i--
 } while ($i -gt 0)
 
 $i = 1
 do {
     Write-Host $i
-    Sleep 300
+    Sleep 60
     $i--
 } while ($i -gt 0)
 
 $i = 1
 do {
     Write-Host $i
-    Sleep 300
+    Sleep 120
+    $i--
+} while ($i -gt 0)
+
+
+$i = 1
+do {
+    Write-Host $i
+    Sleep 180
     $i--
 } while ($i -gt 0)
 
 $i = 1
 do {
     Write-Host $i
-    Sleep 300
+    Sleep 60
     $i--
 } while ($i -gt 0)
+
+$i = 1
+do {
+    Write-Host $i
+    Sleep 120
+    $i--
+} while ($i -gt 0)
+
+
+$i = 1
+do {
+    Write-Host $i
+    Sleep 180
+    $i--
+} while ($i -gt 0)
+
+$i = 1
+do {
+    Write-Host $i
+    Sleep 60
+    $i--
+} while ($i -gt 0)
+
+$i = 1
+do {
+    Write-Host $i
+    Sleep 120
+    $i--
+} while ($i -gt 0)
+
+
+$i = 1
+do {
+    Write-Host $i
+    Sleep 180
+    $i--
+} while ($i -gt 0)
+
+$i = 1
+do {
+    Write-Host $i
+    Sleep 60
+    $i--
+} while ($i -gt 0)
+
+$i = 1
+do {
+    Write-Host $i
+    Sleep 120
+    $i--
+} while ($i -gt 0)
+
+
+$i = 1
+do {
+    Write-Host $i
+    Sleep 180
+    $i--
+} while ($i -gt 0)
+
+$i = 1
+do {
+    Write-Host $i
+    Sleep 60
+    $i--
+} while ($i -gt 0)
+
+$i = 1
+do {
+    Write-Host $i
+    Sleep 120
+    $i--
+} while ($i -gt 0)
+
+
+$i = 1
+do {
+    Write-Host $i
+    Sleep 180
+    $i--
+} while ($i -gt 0)
+
