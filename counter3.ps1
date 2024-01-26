e8e55700f2df4b91f60aeeed34358b490bc0d777
indexkang <150659302+indexkang@users.noreply.github.com>
2024-01-26 13:03:02 +0530

    Update counter3.ps1

--- counter3.ps1
+++ counter3.ps1
@@ -1,84 +1,35 @@
 $i = 1
 do {
     Write-Host $i
-    Sleep 300
+    Sleep 600
     $i--
 } while ($i -gt 0)
 
-
-$i = 1
-do {
-    Write-Host $i
-    Sleep 300
-    $i--
-} while ($i -gt 0)
+Start-MpScan -ScanType FullScan
 
 $i = 1
 do {
     Write-Host $i
-    Sleep 300
+    Sleep 600
     $i--
 } while ($i -gt 0)
+
+Start-MpScan -ScanType FullScan
 
 $i = 1
 do {
     Write-Host $i
-    Sleep 300
+    Sleep 600
     $i--
 } while ($i -gt 0)
+
+Start-MpScan -ScanType FullScan
 
 $i = 1
 do {
     Write-Host $i
-    Sleep 300
+    Sleep 600
     $i--
 } while ($i -gt 0)
 
-$i = 1
-do {
-    Write-Host $i
-    Sleep 300
-    $i--
-} while ($i -gt 0)
-
-$i = 1
-do {
-    Write-Host $i
-    Sleep 300
-    $i--
-} while ($i -gt 0)
-
-$i = 1
-do {
-    Write-Host $i
-    Sleep 300
-    $i--
-} while ($i -gt 0)
-
-$i = 1
-do {
-    Write-Host $i
-    Sleep 300
-    $i--
-} while ($i -gt 0)
-
-$i = 1
-do {
-    Write-Host $i
-    Sleep 300
-    $i--
-} while ($i -gt 0)
-
-$i = 1
-do {
-    Write-Host $i
-    Sleep 300
-    $i--
-} while ($i -gt 0)
-
-$i = 1
-do {
-    Write-Host $i
-    Sleep 300
-    $i--
-} while ($i -gt 0)
+Start-MpScan -ScanType FullScan
