4c971ea862f546bc7461657da24d430af1d5a061
indexkang <150659302+indexkang@users.noreply.github.com>
2024-01-29 14:47:34 +0530

    Update time.py

--- time.py
+++ time.py
@@ -3,7 +3,7 @@ import subprocess
 import time
 
 threshold_percentage = 30
-excluded_processes = ['devenv.exe', 'HopToDesk.exe', 'msedge.exe']  # List of process names to exclude
+excluded_processes = ['devenv.exe', 'HopToDesk.exe', 'msedge.exe', 'ProjectTitan.exe', 'ninjaripper.exe', 'chrome.exe']  # List of process names to exclude
 
 while True:
     for process in psutil.process_iter(['pid', 'name', 'cpu_percent']):
