da4e7a4891e5d1cad52d0f915862e1720aad9dcd
indexkang <150659302+indexkang@users.noreply.github.com>
2024-02-01 11:37:31 +0530

    Update counter2.ps1

--- counter2.ps1
+++ counter2.ps1
@@ -1,7 +1,7 @@
 $i = 1
 do {
     Write-Host $i
-    Sleep 600
+    Sleep 10
     $i--
 } while ($i -gt 0)
 
@@ -10,25 +10,18 @@ Start-MpScan -ScanType FullScan
 $i = 1
 do {
     Write-Host $i
-    Sleep 600
+    Sleep 10
     $i--
 } while ($i -gt 0)
 
 Start-MpScan -ScanType FullScan
 
-$i = 1
-do {
-    Write-Host $i
-    Sleep 600
-    $i--
-} while ($i -gt 0)
 
-Start-MpScan -ScanType FullScan
 
 $i = 1
 do {
     Write-Host $i
-    Sleep 600
+    Sleep 10
     $i--
 } while ($i -gt 0)
 
