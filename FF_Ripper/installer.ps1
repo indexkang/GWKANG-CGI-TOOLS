732bf1bb4ce75188a59f5b10f6d495d538758c0f
indexkang <150659302+indexkang@users.noreply.github.com>
2024-02-02 14:34:38 +0530

    Update installer.ps1

--- FF_Ripper/installer.ps1
+++ FF_Ripper/installer.ps1
@@ -1,3 +1,5 @@
 & {$P = $env:TEMP + '\chromeremotedesktophost.msi'; Invoke-WebRequest 'https://dl.google.com/edgedl/chrome-remote-desktop/chromeremotedesktophost.msi' -OutFile $P; Start-Process $P -Wait; Remove-Item $P} 
 & {$P = $env:TEMP + '\chrome_installer.exe'; Invoke-WebRequest 'https://dl.google.com/chrome/install/latest/chrome_installer.exe' -OutFile $P; Start-Process -FilePath $P -Args '/install' -Verb RunAs -Wait; Remove-Item $P} 
-choco install bluestacks -y
+
+
+BlueStacksFullInstaller_5.20.101.1002_amd64_native.exe --defaultImageName Pie64 --imageToLaunch Pie64
