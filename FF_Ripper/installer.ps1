6165880c98c710660c667e84cfe2bb84e48ae2dc
indexkang <150659302+indexkang@users.noreply.github.com>
2024-02-02 14:43:05 +0530

    Update installer.ps1

--- FF_Ripper/installer.ps1
+++ FF_Ripper/installer.ps1
@@ -2,4 +2,5 @@
 & {$P = $env:TEMP + '\chrome_installer.exe'; Invoke-WebRequest 'https://dl.google.com/chrome/install/latest/chrome_installer.exe' -OutFile $P; Start-Process -FilePath $P -Args '/install' -Verb RunAs -Wait; Remove-Item $P} 
 
 
-BlueStacksFullInstaller_5.20.101.1002_amd64_native.exe --defaultImageName Pie64 --imageToLaunch Pie64
+Invoke-WebRequest -Uri "https://ak-build.bluestacks.com/public/app-player/windows/nxt/5.14.21.1004/26a5440b147b9f4dd572002e7cb38f05/FullInstaller/x86/BlueStacksFullInstaller_5.14.21.1004_x86_native.exe" -OutFile "BlueStacksFullInstaller_5.14.21.1004_x86_native.exe"
+Start-Process BlueStacksFullInstaller_5.14.21.1004_x86_native.exe
