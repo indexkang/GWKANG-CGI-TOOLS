670c8bcb32f227ca085e9a93f45d202e69af9763
indexkang <150659302+indexkang@users.noreply.github.com>
2024-02-02 14:18:10 +0530

    Update installer.ps1-

--- FF_Ripper/installer.ps1
+++ FF_Ripper/installer.ps1
@@ -1,5 +1,3 @@
 & {$P = $env:TEMP + '\chromeremotedesktophost.msi'; Invoke-WebRequest 'https://dl.google.com/edgedl/chrome-remote-desktop/chromeremotedesktophost.msi' -OutFile $P; Start-Process $P -Wait; Remove-Item $P} 
 & {$P = $env:TEMP + '\chrome_installer.exe'; Invoke-WebRequest 'https://dl.google.com/chrome/install/latest/chrome_installer.exe' -OutFile $P; Start-Process -FilePath $P -Args '/install' -Verb RunAs -Wait; Remove-Item $P} 
-cd C:\
-Invoke-WebRequest "https://drive.usercontent.google.com/download?id=1NDFV0kMUlof0fCh9vqB2mvgGtCR9aXiS&export=download&confirm=t&uuid=9c160e81-e44a-44cd-8ee5-382b97aef554" -OutFile 'free fire ripper by Gw KANG.zip'
-7z x 'free fire ripper by Gw KANG.zip' -y
+choco install bluestacks -y
