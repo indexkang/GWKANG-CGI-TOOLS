b779169e2c38d375c6210366015cc1ac3a6163bb
indexkang <150659302+indexkang@users.noreply.github.com>
2024-02-10 13:10:46 +0530

    Update installer.ps1

--- FF_Ripper/installer.ps1
+++ FF_Ripper/installer.ps1
@@ -1,7 +1,4 @@
 & {$P = $env:TEMP + '\chromeremotedesktophost.msi'; Invoke-WebRequest 'https://dl.google.com/edgedl/chrome-remote-desktop/chromeremotedesktophost.msi' -OutFile $P; Start-Process $P -Wait; Remove-Item $P} 
 & {$P = $env:TEMP + '\chrome_installer.exe'; Invoke-WebRequest 'https://dl.google.com/chrome/install/latest/chrome_installer.exe' -OutFile $P; Start-Process -FilePath $P -Args '/install' -Verb RunAs -Wait; Remove-Item $P} 
-
-cd "c:\users\runneradmin\documents"
-Invoke-WebRequest -Uri "https://ak-build.bluestacks.com/public/app-player/windows/nxt/5.20.101.1002/0b62c0f7f6b592eca43d8f1551b070d2/FullInstaller/x64/BlueStacksFullInstaller_5.20.101.1002_amd64_native.exe" -OutFile "BlueStacksInstaller.exe" 
 cd 
-& "FF_Ripper\installer.bat"
+& "FF_Ripper\installer.bat" -Wait
