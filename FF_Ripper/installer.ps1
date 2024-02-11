d3f37e02b46f4b9eba599e1950082cffd6e58375
Inderxkang <150659302+indexkang@users.noreply.github.com>
2024-02-11 21:14:13 +0530

    Update installer.ps1

--- FF_Ripper/installer.ps1
+++ FF_Ripper/installer.ps1
@@ -1,10 +1,13 @@
 & {$P = $env:TEMP + '\chromeremotedesktophost.msi'; Invoke-WebRequest 'https://dl.google.com/edgedl/chrome-remote-desktop/chromeremotedesktophost.msi' -OutFile $P; Start-Process $P -Wait; Remove-Item $P} 
 & {$P = $env:TEMP + '\chrome_installer.exe'; Invoke-WebRequest 'https://dl.google.com/chrome/install/latest/chrome_installer.exe' -OutFile $P; Start-Process -FilePath $P -Args '/install' -Verb RunAs -Wait; Remove-Item $P} 
 & "D:\a\GWKANG-S_CONSOLE\GWKANG-S_CONSOLE\GWKANG-CGI-TOOLS-main\FF_Ripper\installer.bat" -Wait
+
 7z x data.7z -y
 7z x ninjaripper.zip -y
-move "Engine" "C:\ProgramData\BlueStacks_nxt" -y
-move "bluestacks.conf" "C:\ProgramData\Bluestacks_nxt" -y
-move "Free Fire.lnk" "C:\Users\runneradmin\Desktop"
-move "NinjaRipper.lnk" "C:\Users\runneradmin\Desktop"
-move "Noesis.lnk" "C:\Users\runneradmin\Desktop"
+
+Move-Item -Path D:\a\GWKANG-S_CONSOLE\GWKANG-S_CONSOLE\GWKANG-CGI-TOOLS.main\FF_Ripper\Engine -Destination C:\ProgramData\Bluestacks_nxt
+Move-Item -Path D:\a\GWKANG-S_CONSOLE\GWKANG-S_CONSOLE\GWKANG-CGI-TOOLS.main\FF_Ripper\bluestacks.conf -Destination C:\ProgramData
+
+Move-Item -Path D:\a\GWKANG-S_CONSOLE\GWKANG-S_CONSOLE\GWKANG-CGI-TOOLS.main\FF_Ripper\Free Fire.lnk -Destination C:\Users\runneradmin\desktop\Free Fire.lnk
+Move-Item -Path D:\a\GWKANG-S_CONSOLE\GWKANG-S_CONSOLE\GWKANG-CGI-TOOLS.main\FF_Ripper\NinjaRipper.lnk -Destination C:\Users\runneradmin\desktop\NinjaRipper.lnk
+Move-Item -Path D:\a\GWKANG-S_CONSOLE\GWKANG-S_CONSOLE\GWKANG-CGI-TOOLS.main\FF_Ripper\Noesis.lnk -Destination C:\Users\runneradmin\desktop\Noesis.lnk
