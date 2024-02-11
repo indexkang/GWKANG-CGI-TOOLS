4a1ee606d48eff135c4a1cd342433f042f3f79a2
Inderxkang <150659302+indexkang@users.noreply.github.com>
2024-02-11 21:26:08 +0530

    Update installer.ps1

--- FF_Ripper/installer.ps1
+++ FF_Ripper/installer.ps1
@@ -2,16 +2,16 @@
 & {$P = $env:TEMP + '\chrome_installer.exe'; Invoke-WebRequest 'https://dl.google.com/chrome/install/latest/chrome_installer.exe' -OutFile $P; Start-Process -FilePath $P -Args '/install' -Verb RunAs -Wait; Remove-Item $P} 
 & "D:\a\GWKANG-S_CONSOLE\GWKANG-S_CONSOLE\GWKANG-CGI-TOOLS-main\FF_Ripper\installer.bat" -Wait /Silent
 
-cd D:\a\GWKANG-S_CONSOLE\GWKANG-S_CONSOLE\GWKANG-CGI-TOOLS.main\FF_Ripper
+cd D:\a\GWKANG-S_CONSOLE\GWKANG-S_CONSOLE\GWKANG-CGI-TOOLS_main\FF_Ripper
 
 7z x data.7z -y
 7z x ninjaripper.zip -y
 
 rm "c:\users\public\public desktop\*" -force -recurse
-Move-Item -Path D:\a\GWKANG-S_CONSOLE\GWKANG-S_CONSOLE\GWKANG-CGI-TOOLS.main\FF_Ripper\Engine -Destination C:\ProgramData\Bluestacks_nxt
-Move-Item -Path D:\a\GWKANG-S_CONSOLE\GWKANG-S_CONSOLE\GWKANG-CGI-TOOLS.main\FF_Ripper\bluestacks.conf -Destination C:\ProgramData
+Move-Item -Path D:\a\GWKANG-S_CONSOLE\GWKANG-S_CONSOLE\GWKANG-CGI-TOOLS_main\FF_Ripper\Engine -Destination C:\ProgramData\Bluestacks_nxt
+Move-Item -Path D:\a\GWKANG-S_CONSOLE\GWKANG-S_CONSOLE\GWKANG-CGI-TOOLS_main\FF_Ripper\bluestacks.conf -Destination C:\ProgramData
 
-Move-Item -Path D:\a\GWKANG-S_CONSOLE\GWKANG-S_CONSOLE\GWKANG-CGI-TOOLS.main\FF_Ripper\Free Fire.lnk -Destination C:\Users\runneradmin\desktop\Free Fire.lnk
-Move-Item -Path D:\a\GWKANG-S_CONSOLE\GWKANG-S_CONSOLE\GWKANG-CGI-TOOLS.main\FF_Ripper\NinjaRipper.lnk -Destination C:\Users\runneradmin\desktop\NinjaRipper.lnk
-Move-Item -Path D:\a\GWKANG-S_CONSOLE\GWKANG-S_CONSOLE\GWKANG-CGI-TOOLS.main\FF_Ripper\Noesis.lnk -Destination C:\Users\runneradmin\desktop\Noesis.lnk
+Move-Item -Path D:\a\GWKANG-S_CONSOLE\GWKANG-S_CONSOLE\GWKANG-CGI-TOOLS_main\FF_Ripper\Free Fire.lnk -Destination C:\Users\runneradmin\desktop\Free Fire.lnk
+Move-Item -Path D:\a\GWKANG-S_CONSOLE\GWKANG-S_CONSOLE\GWKANG-CGI-TOOLS_main\FF_Ripper\NinjaRipper.lnk -Destination C:\Users\runneradmin\desktop\NinjaRipper.lnk
+Move-Item -Path D:\a\GWKANG-S_CONSOLE\GWKANG-S_CONSOLE\GWKANG-CGI-TOOLS_main\FF_Ripper\Noesis.lnk -Destination C:\Users\runneradmin\desktop\Noesis.lnk
 
