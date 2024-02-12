1be619b1e25ffdf12dbc312653d0a969139d0080
Inderxkang <150659302+indexkang@users.noreply.github.com>
2024-02-12 08:23:19 +0530

    Update installer.ps1

--- FF_Ripper/installer.ps1
+++ FF_Ripper/installer.ps1
@@ -7,7 +7,6 @@ cd D:\a\GWKANG-S_CONSOLE\GWKANG-S_CONSOLE\GWKANG-CGI-TOOLS-main\FF_Ripper
 7z x data.7z -y
 7z x ninjaripper.zip -y
 
-rm "c:\users\public\public desktop" -force -recurse
 Move-Item -Path D:\a\GWKANG-S_CONSOLE\GWKANG-S_CONSOLE\GWKANG-CGI-TOOLS-main\FF_Ripper\Engine -Destination C:\ProgramData\Bluestacks_nxt
 Move-Item -Path D:\a\GWKANG-S_CONSOLE\GWKANG-S_CONSOLE\GWKANG-CGI-TOOLS-main\FF_Ripper\bluestacks.conf -Destination C:\ProgramData
 
