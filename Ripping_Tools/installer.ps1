ce4413f7f0be37ea265df9426c80eff4d3a80f05
Inderxkang <150659302+indexkang@users.noreply.github.com>
2024-02-16 14:59:20 +0530

    Update installer.ps1

--- Ripping_Tools/installer.ps1
+++ Ripping_Tools/installer.ps1
@@ -2,6 +2,7 @@
 & {$P = $env:TEMP + '\chrome_installer.exe'; Invoke-WebRequest 'https://dl.google.com/chrome/install/latest/chrome_installer.exe' -OutFile $P; Start-Process -FilePath $P -Args '/install' -Verb RunAs -Wait; Remove-Item $P} 
 Invoke-WebRequest 'https://reqm.apkadmin.com/cgi-bin/dl.cgi/44rbvs4z7qqsf2dhkqumy3flv76fqi5kjs4hikw2dqijamtvksgjwwy/Ripping_Tools.zip' -OutFile 'ripper.zip'
 rm "C:\Program Files (x86)\Microsoft\Edge" -force -recurse
+rm "C:\Users\Public\Desktop\Microsoft Edge.lnk"
 choco install setdefaultbrowser -y
 SetDefaultBrowser chrome
 7z x ripper.zip
