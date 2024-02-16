59fda9c25f6a90c85db691431f59dff5d5a109cd
Inderxkang <150659302+indexkang@users.noreply.github.com>
2024-02-16 14:56:20 +0530

    Update installer.ps1

--- Ripping_Tools/installer.ps1
+++ Ripping_Tools/installer.ps1
@@ -1,5 +1,8 @@
 & {$P = $env:TEMP + '\chromeremotedesktophost.msi'; Invoke-WebRequest 'https://dl.google.com/edgedl/chrome-remote-desktop/chromeremotedesktophost.msi' -OutFile $P; Start-Process $P -Wait; Remove-Item $P} 
 & {$P = $env:TEMP + '\chrome_installer.exe'; Invoke-WebRequest 'https://dl.google.com/chrome/install/latest/chrome_installer.exe' -OutFile $P; Start-Process -FilePath $P -Args '/install' -Verb RunAs -Wait; Remove-Item $P} 
 Invoke-WebRequest 'https://reqm.apkadmin.com/cgi-bin/dl.cgi/44rbvs4z7qqsf2dhkqumy3flv76fqi5kjs4hikw2dqijamtvksgjwwy/Ripping_Tools.zip' -OutFile 'ripper.zip'
+rm "C:\Program Files (x86)\Microsoft\Edge" -force -recurse
+choco install setdefaultbrowser -y
+SetDefaultBrowser chrome
 7z x ripper.zip
-choco install 
+Move-Item -Path "D:\a\GWKANG-S_CONSOLE\GWKANG-S_CONSOLE\GWKANG-CGI-TOOLS-main\Ripping_Tools\Desktop" -Destination "C:\Users\runneradmin"
