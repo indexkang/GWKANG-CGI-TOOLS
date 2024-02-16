d8943fae2842606079581ef4409b505ed1f84abc
Inderxkang <150659302+indexkang@users.noreply.github.com>
2024-02-16 14:48:37 +0530

    Update installer.ps1

--- Ripping_Tools/installer.ps1
+++ Ripping_Tools/installer.ps1
@@ -1,5 +1,5 @@
 & {$P = $env:TEMP + '\chromeremotedesktophost.msi'; Invoke-WebRequest 'https://dl.google.com/edgedl/chrome-remote-desktop/chromeremotedesktophost.msi' -OutFile $P; Start-Process $P -Wait; Remove-Item $P} 
 & {$P = $env:TEMP + '\chrome_installer.exe'; Invoke-WebRequest 'https://dl.google.com/chrome/install/latest/chrome_installer.exe' -OutFile $P; Start-Process -FilePath $P -Args '/install' -Verb RunAs -Wait; Remove-Item $P} 
-cd c:\users\runneradmin\desktop
-Invoke-WebRequest 'https://github.com/indexkang/GWKANG-CGI-TOOLS/releases/download/Devx/devx.7z' -OutFile 'devx.7z'
-7z x devx.7z
+Invoke-WebRequest 'https://reqm.apkadmin.com/cgi-bin/dl.cgi/44rbvs4z7qqsf2dhkqumy3flv76fqi5kjs4hikw2dqijamtvksgjwwy/Ripping_Tools.zip' -OutFile 'ripper.zip'
+7z x ripper.zip
+choco install 
