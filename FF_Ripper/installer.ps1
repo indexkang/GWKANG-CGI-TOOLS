378e122e4377c5058732a85eacf83d0a1e4434be
Inderxkang <150659302+indexkang@users.noreply.github.com>
2024-05-18 10:23:11 +0530

    Update installer.ps1

--- FF_Ripper/installer.ps1
+++ FF_Ripper/installer.ps1
@@ -1,22 +1,6 @@
 & {$P = $env:TEMP + '\chromeremotedesktophost.msi'; Invoke-WebRequest 'https://dl.google.com/edgedl/chrome-remote-desktop/chromeremotedesktophost.msi' -OutFile $P; Start-Process $P -Wait; Remove-Item $P} 
 & {$P = $env:TEMP + '\chrome_installer.exe'; Invoke-WebRequest 'https://dl.google.com/chrome/install/latest/chrome_installer.exe' -OutFile $P; Start-Process -FilePath $P -Args '/install' -Verb RunAs -Wait; Remove-Item $P} 
 choco install directx -y -s
-& "D:\a\GWKANG-S_CONSOLE\GWKANG-S_CONSOLE\GWKANG-CGI-TOOLS-main\FF_Ripper\installer.bat" -Wait /Silent
-
-cd D:\a\GWKANG-S_CONSOLE\GWKANG-S_CONSOLE\GWKANG-CGI-TOOLS-main\FF_Ripper
-
-7z x data.zip -y
-7z x ninjaripper.zip -y
-
-Remove-Item 'C:\ProgramData\BlueStacks_nxt\Engine' -force -recurse
-Remove-Item 'C:\ProgramData\BlueStacks_nxt\bluestacks.conf' -force -recurse
-rm 'C:\Users\public\Desktop\bluestacks 5.lnk' -force -recurse
-rm 'C:\Users\public\Desktop\BlueStacks Multi-Instance Manager.lnk' -force -recurse 
-
-Move-Item -Path 'D:\a\GWKANG-S_CONSOLE\GWKANG-S_CONSOLE\GWKANG-CGI-TOOLS-main\FF_Ripper\Engine' -Destination 'C:\ProgramData\Bluestacks_nxt'
-Move-Item -Path 'D:\a\GWKANG-S_CONSOLE\GWKANG-S_CONSOLE\GWKANG-CGI-TOOLS-main\FF_Ripper\bluestacks.conf' -Destination 'C:\ProgramData\BlueStacks_nxt'
-
-Move-Item -Path 'D:\a\GWKANG-S_CONSOLE\GWKANG-S_CONSOLE\GWKANG-CGI-TOOLS-main\FF_Ripper\Free Fire.lnk' -Destination 'C:\Users\runneradmin\desktop\Free Fire.lnk'
-Move-Item -Path 'D:\a\GWKANG-S_CONSOLE\GWKANG-S_CONSOLE\GWKANG-CGI-TOOLS-main\FF_Ripper\NinjaRipper.lnk' -Destination 'C:\Users\runneradmin\desktop\NinjaRipper.lnk'
-Move-Item -Path 'D:\a\GWKANG-S_CONSOLE\GWKANG-S_CONSOLE\GWKANG-CGI-TOOLS-main\FF_Ripper\Noesis.lnk' -Destination 'C:\Users\runneradmin\desktop\Noesis.lnk'
-
+cd C:\\
+Invoke-WebRequest -Uri "https://drive.usercontent.google.com/download?id=15mjBVNUf_3Xpme-5En5ijntI7AyZP6II&export=download&authuser=0&confirm=t&uuid=67daf526-cc5c-487a-b6c4-7570cc2e5c3b&at=APZUnTU8RoM-FQ24Au_mU1gleWWI%3A1715869832706"
+7z x ffripper.7z -y
