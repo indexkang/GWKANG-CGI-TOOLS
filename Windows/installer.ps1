d6eeee1d753734b40cd100be61089e7a398c6b27
Inderxkang <150659302+indexkang@users.noreply.github.com>
2024-05-18 21:31:02 +0530

    Update installer.ps1

--- Windows/installer.ps1
+++ Windows/installer.ps1
@@ -1,2 +1,2 @@
-& {$P = $env:TEMP + '\chromeremotedesktophost.msi'; Invoke-WebRequest 'https://dl.google.com/edgedl/chrome-remote-desktop/chromeremotedesktophost.msi' -OutFile $P; Start-Process $P -Wait; Remove-Item $P} 
-& {$P = $env:TEMP + '\chrome_installer.exe'; Invoke-WebRequest 'https://dl.google.com/chrome/install/latest/chrome_installer.exe' -OutFile $P; Start-Process -FilePath $P -Args '/install' -Verb RunAs -Wait; Remove-Item $P} 
+choco install chrome-remote-desktop-host -y
+choco install googlechrome -y
