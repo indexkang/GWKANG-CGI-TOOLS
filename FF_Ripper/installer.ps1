6ad0ff8961bf0e1de3e279429a7e4d6af520dadc
Inderxkang <150659302+indexkang@users.noreply.github.com>
2024-05-23 17:14:25 +0530

    : )

--- FF_Ripper/installer.ps1
+++ FF_Ripper/installer.ps1
@@ -1,5 +1,14 @@
-choco install chrome-remote-desktop-host googlechrome -y
-choco install directx -y -s
-cd C:\\
-Invoke-WebRequest -Uri "https://drive.usercontent.google.com/download?id=15mjBVNUf_3Xpme-5En5ijntI7AyZP6II&export=download&authuser=0&confirm=t&uuid=67daf526-cc5c-487a-b6c4-7570cc2e5c3b&at=APZUnTU8RoM-FQ24Au_mU1gleWWI%3A1715869832706"
-7z x ffripper.7z -y
+& {Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False}
+choco install chrome-remote-desktop-host -y
+choco install directx -y
+choco install googlechrome -y
+cd "D:\\a\FreeFireRipper\FreeFireRipper"
+cd C:\Users\$Env:USERNAME\Documents
+Invoke-WebRequest -Uri "https://drive.usercontent.google.com/download?id=11EUFz6hufNT1g56-CmOJw9_KxMERZxhe&export=download&authuser=0&confirm=t&uuid=157ad075-6074-4fb7-ade2-63e76e7956b7&at=APZUnTUkxLXjKC5Aliqg_3gGGgPu%3A1716557813235" -Outfile ffripper.zip
+7z x ffripper.zip -y
+Start-Process "C:\Users\$Env:USERNAME\Documents\noesis\Noesis64.exe"
+Start-Process "C:\Users\$Env:USERNAME\Documents\ninjaripper\x86\ninjaripper.exe"
+Start-Process .
+New-Item -ItemType SymbolicLink -Target "C:\Users\$Env:USERNAME\Documents\FenixGaga\Engine\ProjectTitan.exe" -Path "C:\Users\$Env:USERNAME\Desktop\Free Fire.lnk"
+New-Item -ItemType SymbolicLink -Target "C:\Users\$Env:USERNAME\Documents\ninjaripper\x86\NinjaRipper.exe" -Path "C:\Users\$Env:USERNAME\Desktop\Ninjaripper.lnk"
+New-Item -ItemType SymbolicLink -Target "C:\Users\$Env:USERNAME\Documents\noesis\Noesis64.exe" -Path "C:\Users\$Env:USERNAME\Desktop\Noesis.lnk"
