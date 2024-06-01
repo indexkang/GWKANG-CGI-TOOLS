1a948a0d636db277df9e4c5b4d70bd3e64ba7e06
Inderxkang <150659302+indexkang@users.noreply.github.com>
2024-06-01 11:10:23 +0530

    Update installer.ps1

--- Ripping_Tools/installer.ps1
+++ Ripping_Tools/installer.ps1
@@ -1,6 +1,6 @@
 choco install chrome-remote-desktop-host googlechrome -y
 cd "C:\Program Files"
-Invoke-WebRequest 'https://drive.usercontent.google.com/download?id=1GV-CUdMruK5q2rDX0b4-YKulcsw3eI7Q&export=download&authuser=0&confirm=t&uuid=66d9c469-d323-41d0-898a-99a26a08855f&at=APZUnTUNYSoUbfNTfd5Po6hy8MBf%3A1717218847853' -OutFile 'ripper.zip'
+Invoke-WebRequest 'https://drive.usercontent.google.com/download?id=1GV-CUdMruK5q2rDX0b4-YKulcsw3eI7Q&export=download&authuser=0&confirm=t&uuid=594acf70-3984-4864-ba22-beab54672462&at=APZUnTV2HHabEcYTJmtLUwwtvw4m%3A1717220341939' -OutFile 'ripper.zip'
 rm "C:\Program Files (x86)\Microsoft\Edge" -force -recurse
 choco install setdefaultbrowser -y
 SetDefaultBrowser chrome
