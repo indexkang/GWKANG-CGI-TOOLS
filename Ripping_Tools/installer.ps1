c34ab329c441bca5cc5ee1ff0bb1b3f59fbbdbd9
Inderxkang <150659302+indexkang@users.noreply.github.com>
2024-06-01 11:04:37 +0530

    Update installer.ps1

--- Ripping_Tools/installer.ps1
+++ Ripping_Tools/installer.ps1
@@ -1,7 +1,12 @@
 choco install chrome-remote-desktop-host googlechrome -y
-Invoke-WebRequest 'https://reqm.apkadmin.com/cgi-bin/dl.cgi/44rbvs4z7qqsf2dhkqumy3flv76fqi5kjs4hikw2dqijamtvksgjwwy/Ripping_Tools.zip' -OutFile 'ripper.zip'
+cd "C:\Program Files"
+Invoke-WebRequest 'https://drive.usercontent.google.com/download?id=1GV-CUdMruK5q2rDX0b4-YKulcsw3eI7Q&export=download&authuser=0&confirm=t&uuid=66d9c469-d323-41d0-898a-99a26a08855f&at=APZUnTUNYSoUbfNTfd5Po6hy8MBf%3A1717218847853' -OutFile 'ripper.zip'
 rm "C:\Program Files (x86)\Microsoft\Edge" -force -recurse
 choco install setdefaultbrowser -y
 SetDefaultBrowser chrome
 7z x ripper.zip
-Move-Item -Path "D:\a\GWKANG-S_CONSOLE\GWKANG-S_CONSOLE\GWKANG-CGI-TOOLS-main\Ripping_Tools\Desktop" -Destination "C:\Users\runneradmin"
+New-Item -ItemType SymbolicLink -Path "C:\Users\$Env:USERNAME\Desktop\AssetsRipper.lnk" -Target "C:\Program Files\AssetRipper\AssetRipper.GUI.Free.exe"
+New-Item -ItemType SymbolicLink -Path "C:\Users\$Env:USERNAME\Desktop\AssetsStudio.lnk" -Target "C:\Program Files\AssetStudio\AssetStudioModGUI.exe"
+New-Item -ItemType SymbolicLink -Path "C:\Users\$Env:USERNAME\Desktop\DevXUnpacker.lnk" -Target "C:\Program Files\noesisv\Noesis64.exe"
+New-Item -ItemType SymbolicLink -Path "C:\Users\$Env:USERNAME\Desktop\Uabea.lnk" -Target "C:\Program Files\uabea\UABEAvalonia.exe"
+New-Item -ItemType SymbolicLink -Path "C:\Users\$Env:USERNAME\Desktop\Noesis.lnk" -Target "C:\Program Files\DevXCrack\DevxUnityUnpackerRun.exe"
